92c9ab829c1bcc0889c4e8e4fb65e3f88f990e71
yj <1614477422@qq.com>
2024-08-27 15:58:51 +0800

    成功改掉第一副牌

--- hk/dllmain.cpp
+++ hk/dllmain.cpp
@@ -11,9 +11,9 @@
 #include <tchar.h> 
 #include <winternl.h>
 
-TCHAR exe[16] = L"360.exe";
+TCHAR exe[16] = L"GServer.exe";
 char hkdll[] = "hk.dll";
-char dll[] = "add.dll";
+char dll[] = "17090400.dll";
 // 定义原始函数类型和函数指针
 typedef void (* func)();
 func OriginalFunction = NULL;
@@ -22,20 +22,24 @@ func OriginalFunction = NULL;
 
 #define HOOK_SIZE 5  // 5字节用于存放跳转指令
 DWORD oldcode;
+DWORD ebpValue;
+BYTE* targetAddress;
 
 bool load()
 {
+	//MessageBoxA(NULL, dll, "load dll", MB_OK);
 	HMODULE hModule = LoadLibraryA(dll);
 	if (!hModule) {
 		std::cerr << "Failed to load DLL code:" << GetLastError() <<std::endl;
+		MessageBoxA(NULL, "Failed to load DLL", "load dll", MB_OK);
 		return false;
 	}
 	// 计算函数地址，加上偏移
-	DWORD offset = 0x1003;//0x10001000
+	DWORD offset = 0x7F1C;//0x7F26   0x7F8B
 	OriginalFunction = (func)((DWORD_PTR)hModule + offset);
 	// 保存目标地址处的原始指令
 	//memcpy(originalCode, (LPVOID)((DWORD_PTR)hModule + offset +5), 5);
-	oldcode = (DWORD)OriginalFunction + 5;
+	oldcode = (DWORD)OriginalFunction + 6;//10  6
 	// 检查函数地址
 	if (!OriginalFunction) {
 		std::cerr << "Failed to get function address" << std::endl;
@@ -45,19 +49,122 @@ bool load()
 	std::cerr << "success load " << dll << std::endl;
 	return true;
 }
+
+//char buffer[100] = { 0 }; // 用于存储18字节的内容
+//unsigned char* buffer = NULL;
+
+// 外部函数，用于显示内容
+void ShowMemoryContent(const DWORD* buffer)
+{
+	targetAddress = (BYTE*)buffer;
+	char hexString[100] = {0}; // 18字节的内容将转换为36字符的十六进制字符串
+	for (int i = 0; i < 18; i++) {
+		sprintf_s(hexString + i * 2, 3, "%02X,", (targetAddress+i));
+	}
+	// 显示结果
+	MessageBoxA(NULL, hexString, "Memory Content", MB_OK);
+}
+unsigned char newData[18] = {
+	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12
+};
 // Hook 函数
 void __declspec(naked)  HookedFunction() {
-	__asm {
-		; mov     edx, [ecx + 8]; 恢复被破坏的代码
-		; test    edx, edx
-		mov     ecx, [ebp + 8]
-		test    ecx, ecx
+	//__asm {
+	//	pushad
+	//}
+	//MessageBoxA(NULL, "hooked", "Memory Content", MB_OK);
+	//__asm {
+	//	popad
+	//}
 
-		add [ecx + 8], 4; 修改循环次数
+
+	__asm {
+		// 保存所有通用寄存器
+		pushad
+		mov ecx, dword ptr ss:[ebp - 0x0018]
+		lea edx, dword ptr ds:[ecx + 0x26CA]
+
+		mov edi, edx
+		// 将 newData 的地址加载到 EDI 寄存器
+		lea esi, newData
+		// 复制 4 个双字 (16 字节)
+		movsd                 // 复制第一个双字
+		movsd                 // 复制第二个双字
+		movsd                 // 复制第三个双字
+		movsd                 // 复制第四个双字
+		// 复制剩余的 2 个字节
+		movsw                 // 复制一个字
+
+		//mov edi, edx
+		//mov byte ptr[edx + 0], 0x01
+		//mov byte ptr[edx + 1], 0x02
+		//mov byte ptr[edx + 2], 0x03
+		//mov byte ptr[edx + 3], 0x04
+		//mov byte ptr[edx + 4], 0x05
+		//mov byte ptr[edx + 5], 0x06
+		//mov byte ptr[edx + 6], 0x07
+		//mov byte ptr[edx + 7], 0x08
+		//mov byte ptr[edx + 8], 0x09
+		//mov byte ptr[edx + 9], 0x0A
+		//mov byte ptr[edx + 10], 0x0B
+		//mov byte ptr[edx + 11], 0x0C
+		//mov byte ptr[edx + 12], 0x0D
+		//mov byte ptr[edi + 13], 0x0E
+		//mov byte ptr[edx + 14], 0x0F
+		//mov byte ptr[edx + 15], 0x10
+		//mov byte ptr[edx + 16], 0x11
+		//mov byte ptr[edx + 17], 0x12
+
+		popad
+
+		//mov eax, [ebp - 0x374]; 恢复被破坏的代码
+
+		mov dword ptr ss:[ebp - 0x04A8], eax
+
 		jmp oldcode; 跳回到原始代码后面的位置
+
 	}
-	//OriginalFunction();
+	//
+	//__asm {
+	//	push edx
+	//	mov edx, [ebp - 18]  // 获取当前EBP寄存器的值
+	//	mov ebpValue, edx
+	//	pop edx
+	//}
+	//__asm {
+	//	pushad
+	//	pushfd
+	//}
+	//// 计算 [ebp - 0x0018 + 0x26CA] 的地址
+	//targetAddress = (BYTE*)(ebpValue);
+
+	////// 读取18字节内容
+	//BYTE buffer[18];
+	//for (int i = 0; i < 18; i++) {
+	//	buffer[i] = targetAddress[i];
+	//}
+
+	////// 将字节内容转换为可显示的字符串
+	//char hexString[100]; // 18字节的内容将转换为36字符的十六进制字符串
+	//for (int i = 0; i < 18; i++) {
+	//	sprintf_s(hexString + i * 2, 3, "%02X,", buffer[i]);
+	//}
+
+	////// 显示结果
+	//MessageBoxA(NULL, hexString, "Memory Content", MB_OK);
+	//__asm {
+	//	popfd
+	//	popad
+	//}
+
+	// 执行原始指令: mov [ebp - 0x0374], 0
+	//__asm {
+	//	mov [ebp - 0x0374], 0; 恢复被破坏的代码
+	//	jmp oldcode; 跳回到原始代码后面的位置
+	//}
 }
+
+
 
 void AttachHooks() {
 	// Detour transaction to attach our hook
